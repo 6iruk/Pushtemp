9536a8be59923ba3adce1e2b24ac1eee3661fec6
Biruk Tamiru <biruktarega@gmail.com>
2017-10-26 23:16:10 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -123,7 +123,8 @@ class Material(models.Model):
       return self.file.name.split('.')[-1]
    def file_size(self):
       size_bytes = self.file.size
-      return size_bytes
+      i = float(size_bytes/1024)
+      return i
    def inc_count(self):
       self.count = self.count + 1
       self.save()
