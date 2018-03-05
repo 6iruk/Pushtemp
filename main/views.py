3e28270a5a9fd5013643022135b34e6238814ba1
addismiraph <genioaddis@gmail.com>
2018-03-05 16:00:57 +0300

    fix syntax error

--- main/views.py
+++ main/views.py
@@ -137,7 +137,7 @@ def first_login(request):
         if (not(request.user.is_authenticated()) or Teacher.objects.filter(user=request.user).exists()):
                 if(request.GET.get('username','') != '' and request.GET.get('password','') != ''):
                         username = request.GET['username']
-                        password = request.GET.['password']
+                        password = request.GET['password']
                         return HttpResponse('<h1>We are here!!!</h1>')
                         user = authenticate(username=username, password=password)
                         if user is not None:
