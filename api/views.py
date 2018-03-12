a37f87a0348a18202515410ff947273bb8c5ba99
6iruk <biruktarega@gmail.com>
2018-03-12 19:56:48 +0300

    Fix api 5

--- api/views.py
+++ api/views.py
@@ -21,8 +21,9 @@ def index(request):
 def courses(request):
         if request.GET.get('sections'):
                 section_codes = request.GET.get('sections').split('-')
-                courses = [Section.objects.get(id=int(x)).take.all() for x in section_codes]
-
+                courses_list = [Section.objects.get(id=int(x)).take.all() for x in section_codes]
+                courses = [y for x in courses_list for y in x]
+                
                 if request.GET.get('department'):
                         courses = courses.filter(department__id=int(request.GET.get('department')))
                         
@@ -31,7 +32,7 @@ def courses(request):
 
         else :
                 return HttpResponse("Incorrect API request format. Refer to the docmumentaion.")
-
+        
         #JSON output
         #Start json array
         output = "[";
