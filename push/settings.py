622edc5599ea46af6f9a59c00da3ca1109184b44
6iruk <biruktarega@gmail.com>
2018-11-19 20:33:20 +0300

    Change db backend

--- push/settings.py
+++ push/settings.py
@@ -79,17 +79,17 @@ WSGI_APPLICATION = 'push.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    
-##    'default': {
-##        'ENGINE': 'django.db.backends.mysql',
-##        'OPTIONS': {
-##            'read_default_file': os.path.join(BASE_DIR, 'push.cnf'),
-##        }
-##    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'OPTIONS': {
+           'read_default_file': os.path.join(BASE_DIR, 'push.cnf'),
+       }
+   }
 }
 
 
